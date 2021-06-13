e6f05feb2fcd568540c1ac859993144c48e06481
Maximilian Dazer <maximilian.dazer@gmx.de>
2021-06-13 14:32:05 +0000

    added css and js for base template

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+// Hamburger anumation code taken from https://jonsuh.com/hamburgers/
+var hamburger = document.querySelector(".hamburger");
+    // On click
+hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+        // Do something else, like open/close menu
+});
